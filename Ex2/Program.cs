1bb7a682b499d4f3f1e6ec1623710de02fe58058
JustCheka <cheka201@yandxe.ru>
2023-08-26 20:48:34 +0300

    Ex 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите m ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите n ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] arr = new int[m,n];
+
+int maxStr = 0;
+for (int i = 0; i < arr.GetLength(0); i++){
+    int sum = 0;
+    for (int j = 0; j < arr.GetLength(1); j++){
+        arr[i,j] = new Random().Next(0,10);
+        Console.Write(arr[i,j] + " ");
+        sum += arr[i,j];
+    }
+    Console.WriteLine();
+    if (i == 0) {
+        maxStr = sum;
+    }
+    else {
+        if (sum > maxStr){
+            maxStr = sum;
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine(maxStr);
